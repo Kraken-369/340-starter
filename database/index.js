0cab3db06355bb9ea1267a4adbb7944693705542
Nestor Otondo <oto23001@byui.edu>
2024-11-20 14:56:36 -0300

    Testing connection

--- database/index.js
+++ database/index.js
@@ -7,31 +7,34 @@ require('dotenv').config()
  * If - else will make determination which to use
  * *************** */
 let pool
+console.log('Inside base controller')
+
 if (process.env.NODE_ENV == 'development') {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
-})
+  })
 
-// Added for troubleshooting queries
-// during development
-module.exports = {
-  async query(text, params) {
-    try {
-      const res = await pool.query(text, params)
-      console.log('executed query', { text })
-      return res
-    } catch (error) {
-      console.error('error in query', { text })
-      throw error
-    }
-  },
-}
+  // Added for troubleshooting queries
+  // during development
+  module.exports = {
+    async query(text, params) {
+      try {
+        const res = await pool.query(text, params)
+        console.log('executed query', { text })
+        return res
+      } catch (error) {
+        console.error('error in query', { text })
+        throw error
+      }
+    },
+  }
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
   })
+  console.log('On production environment: ', process.env.DATABASE_URL)
   module.exports = pool
 }
