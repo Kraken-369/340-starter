9b3bc86102103c43e94ff691e7de443ab43b2c22
Nestor Otondo <nestor.otondo@losmarketeros.com.br>
2024-11-27 23:08:40 -0300

    Adding session access

--- server.js
+++ server.js
@@ -13,6 +13,26 @@ const static = require('./routes/static')
 const inventoryRoute = require('./routes/inventoryRoute')
 const baseController = require('./controllers/baseController')
 const utilities = require('./utilities/')
+const session = require("express-session")
+const pool = require('./database/')
+
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
