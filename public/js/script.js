99aa143178c5f18550a9b022a06f2fb0f5cce1b1
Nestor Otondo <oto23001@byui.edu>
2024-12-04 19:29:44 -0300

    Adding hamburger button to responsive nav menu

---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+/**
+ *  Toggle Menu to responsive mobile screen 
+ */
+
+document.getElementById('current-year').innerText = new Date().getFullYear();
+
+const menu = document.getElementById('main-menu');
+const hamburgerButton = document.querySelector('.hamburger-button');
+console.log(menu);
+
+const toggleMenu = () => {
+  if (menu.classList.contains('show-menu')) {
+    menu.classList.remove('show-menu');
+  } else {
+    menu.classList.add('show-menu');
+  }
+
+}
+
+hamburgerButton.addEventListener('click', toggleMenu);
